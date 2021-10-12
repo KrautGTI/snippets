fbcce9c4f8a41fc357a98731fd3e289f02aa1a42
Julian Mifsud <jmif0048@gmail.com>
2021-10-12 22:34:59 +0200

    nodemodules import

---
+++ index.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import cheerio from 'cheerio';
+import express from 'express';
+const PORT = 8000;
+const express2 = require('express');
+const app2 = express2();
+const app = express();
+
+app2.listen
+app.listen(PORT, () => console.log("server running on"&PORT));
