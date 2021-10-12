8b8c7a4cecb96c7627a850feee1d8e0515beaad4
Julian Mifsud <jmif0048@gmail.com>
2021-10-13 01:02:45 +0200

    Completed Scraping Scripts

--- index.js
+++ index.js
@@ -1,10 +1,54 @@
-import axios from 'axios';
-import cheerio from 'cheerio';
-import express from 'express';
-const PORT = 8000;
-const express2 = require('express');
-const app2 = express2();
+const express = require('express');
+const axios = require('axios') 
+const cheerio = require('cheerio'); 
+
+
 const app = express();
 
-app2.listen
-app.listen(PORT, () => console.log("server running on"&PORT));
+module.exports.theGuardian = function scrape1(){ 
+const url = 'https://www.theguardian.com/uk'
+
+axios(url)
+    .then(response => {
+        const html = response.data
+        const c = cheerio.load(html)
+        const articles = []
+        c('.fc-item__title',html).each(function()  {
+            const title = c(this).text()
+            const link = c(this).find('a').attr('href')
+            articles.push({
+                title,
+                link
+            });
+        })
+        console.log(articles)
+    
+    }).catch(err => console.log(err))
+
+}
+
+module.exports.cb = function scrape2(){
+const url = 'https://cb.com/'
+
+    axios(url)
+        .then(response => {
+            const html = response.data
+            const c = cheerio.load(html)
+            const articles = []
+            c('.room_list_room',html).each(function()  {
+                const age = c(this).find('.age').text()
+                const username = c(this).find('a').attr('data-room')
+                const location = c(this).find('.location').text()
+                const cams = c(this).find('.cams').text()
+                articles.push({
+                    username,
+                    age,
+                    location,
+                    cams
+            })
+    })
+            console.log(articles)
+        
+        }).catch(err => console.log(err))
+    
+    }
