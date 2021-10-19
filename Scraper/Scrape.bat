8d611d1009c76381051376c101f06630f9604701
Julian Mifsud <jmif0048@gmail.com>
2021-10-20 00:41:27 +0200

    Update Scraper paths

--- Scraper/Scrape.bat
+++ Scraper/Scrape.bat
@@ -1,2 +1,2 @@
-cd "C:\Users\Julian\Dropbox\School Works\Notepad Items\GitHub\WebScraper"
+cd "C:\Users\Julian\Dropbox\School Works\Notepad Items\GitHub\Snippets\Scraper"
 npm run start > "C:\Users\Julian\Desktop\ScrapeResults.json"
