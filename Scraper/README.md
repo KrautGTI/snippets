eb20f1cd06e9d7c033b16165abb4f346c6b94870
Julian Mifsud <jmif0048@gmail.com>
2021-11-25 18:51:12 +0100

    Update with some Scraper Testcases - Podcasts. WIP

--- Scraper/README.md
+++ Scraper/README.md
@@ -1,2 +1,7 @@
 # web-scraper
 Web Scraper
+
+To add a new script:
+- Create new function
+- Include the new in package.json under scripts to be called there. 
+- npm run <function name> eg: npm run earBiscuits.
--- Scraper/scraper.js
+++ Scraper/scraper.js
@@ -2,7 +2,6 @@ const express = require('express');
 const axios = require('axios') 
 const cheerio = require('cheerio'); 
 
-
 const app = express();
 
 module.exports.theGuardian = function scrape1(){ 
@@ -13,7 +12,7 @@ axios(url)
         const html = response.data
         const c = cheerio.load(html)
         const articles = []
-        c('.fc-item__title',html).each(function()  {
+        c('.fc-item__title',html).each(function(){
             const title = c(this).text()
             const link = c(this).find('a').attr('href')
             articles.push({
@@ -52,3 +51,74 @@ const url = 'https://cb.com/'
         }).catch(err => console.log(err))
     
     }
+
+module.exports.earBiscuits = function scrape3(){ 
+    const url = 'https://www.stitcher.com/show/ear-biscuits-with-rhett-and-link'
+    
+    axios(url)
+        .then(response => {
+            const html = response.data
+
+            const c = cheerio.load(html)
+            const episodes = []
+            c('.v-window__container', html).each(function () {
+                const overview = c(this).text();
+                episodes.push({
+                    overview
+                });
+            })
+            console.log(episodes)
+        
+        }).catch(err => console.log(`Error: ${err}`))
+}
+
+module.exports.hotDog = function scrape4(){ 
+    const url = 'https://podcasts.apple.com/gb/podcast/a-hot-dog-is-a-sandwich/id1500763931'
+    
+    axios(url)
+        .then(response => {
+            const html = response.data
+
+            const c = cheerio.load(html)
+            const episodes = []
+            c('ol', html).each(function () {
+               // const overview = c(this).text();
+                const date = c(this).find('time').text();
+                const title = c(this).find(".we-clamp we-clamp--visual").text();
+
+
+                episodes.push({
+                  //  overview,
+                    title,
+                    date
+                });
+            })
+            console.log(episodes)
+        
+        }).catch(err => console.log(`Error: ${err}`))
+}
+
+module.exports.hotDog2 = function scrape5(){ 
+    const url = 'https://www.rambleofficial.com/hotdog.html'
+    
+    axios(url)
+        .then(response => {
+            const html = response.data
+
+            const c = cheerio.load(html)
+            const episodes = []
+            c('.playlist sc-jTzLTM itEsdV', html).each(function () {
+                const overview = c(this).text();
+                const date = c(this).find('.date-and-duration').text();
+                const title = c(this).find('.title').text();
+
+                episodes.push({
+                    overview,
+                    title,
+                    date
+                });
+            })
+            console.log(episodes)
+        
+        }).catch(err => console.log(`Error: ${err}`))
+}
